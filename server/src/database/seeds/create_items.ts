c0c360d406b89b2b33718729a2818c85249dbe5d
DavidsonIzefler <davidson.izefler@gmail.com>
2020-06-06 22:43:37 -0300

    Translation

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -2,11 +2,11 @@ import Knex from 'knex';
 
 export async function seed(knex: Knex) {
   await knex('items').insert([
-    { title: 'Lampadas', image: 'lampadas.svg'},
-    { title: 'Pilhas e Baterias', image: 'baterias.svg'},
-    { title: 'Papeis e Papelao', image: 'papeis-papelao.svg'},
-    { title: 'Residuos Eletronicos', image: 'eletronicos.svg'},
-    { title: 'Residuos Organicos', image: 'organicos.svg'},
-    { title: 'Oleo de cozinha', image: 'oleo.svg'},
+    { title: 'Lamps', image: 'lampadas.svg'},
+    { title: 'Batteries', image: 'baterias.svg'},
+    { title: 'Papers', image: 'papeis-papelao.svg'},
+    { title: 'Electronic waste', image: 'eletronicos.svg'},
+    { title: 'Organic waste', image: 'organicos.svg'},
+    { title: 'Cooking oil', image: 'oleo.svg'},
   ]);
-}
+} 
